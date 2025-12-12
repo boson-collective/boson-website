fae93aaac1c77d338e66fd5f0ec2c9641ede4284
mmacnesa <mmacnesa@gmail.com>
2025-12-12 23:21:20 +0700

    komit

--- client/app/home/page.jsx
+++ client/app/home/page.jsx
@@ -19,7 +19,7 @@ import Services from '../../components/organisms/Services'
 const SoleNoir = dynamic(() => import("../../components/organisms/SoleNoir"), { ssr: false });
 import Beginning from '../sandbox/13/page'
 
-import Chadash from '../sandbox/28/page'
+import Chadash from '../sandbox/29/page'
 
 export default function Page() {
   return (
